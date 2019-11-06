39f0864320993a347557c3b3f675f3c5787fa6ce
Yulia Shutova <usshutova@gmail.com>
2019-11-07 01:16:39 +0300

    Edit form is OK

--- js/index.js
+++ js/index.js
@@ -28,217 +28,195 @@ $(document).ready(function(){
 
 	const ENTER_KEY = 13;
 
-	const deliveryInfo = new Map([
-		[
-			"Russia",
-			["Moscow", "St. Petersburg", "Saratov", "Samara"]
-		],
-		[
-			"Belarus",
-			["Minsk", "Grodno", "Gomilev"]
-		],
-		[
-			"USA",
-			["New-York", "Chicago", "Siettle"]
-		]
-		]);
-
-
 	class List{
-		constructor(){
-			this.productList = new Map;
-			this._idCounter = 0;
-			// this.deliveryInfo = deliveryInfo;
-
-			this.productList.set(
-				this._idCounter++,
-				{
-				name: "Pencil",
-				email: "uss@gmail.com",
-				count: 7,
-				price: 5432.6533
-				});
-			this.productList.set(
-				this._idCounter++,
-				{
-				name: "Pen",
-				email: "abcfs12@gmail.com",
-				count: 443,
-				price: 7434322.654
-			});
-			this.productList.set(
-				this._idCounter++,
-				{
-				name: "Adonit",
-				email: "asfbf@gmail.com",
-				count: 467,
-				price: 6543.00000
-			})
-			this.render();
+		constructor(newList = 0){
+			if (newList)
+				this.#init(newList);
 		}
 
-		addProduct(el){
-			this.productList.set(
-				this._idCounter++,
-				el);
+		#idCounter = 0;
+		#productList = {};
+
+		#init = function(newList){
+			for(let id in newList){
+				this.#productList[this.#idCounter++] = newList[id];
+			}
+		}
+
+		addProduct(el, id = -1){
+			if (id === -1)
+				this.#productList[this.#idCounter++] = el;
+			else{
+				delete this.#productList[id];
+				this.#productList[id] = el;
+			}
 			return new Promise((resolve, reject) => {
 				setTimeout(() => resolve(this), 2000);
 			})
 		}
 
 		deleteProduct(id){
-			this.productList.delete(parseInt(id, 10));
+			delete this.#productList[id];
 			return new Promise((resolve, reject) => {
-				$("body").css("cursor", "progress");
 				setTimeout(() => resolve(this), 2000);
 			})
 		}
 
-		// getProduct(id){
-		// 	return this.productList.get(id);
-		// }
+		getProductValue(id, key){
+			if(key==="delivery-country"){
+				// console.log("id (getProductValue) = " + id + "(" + typeof id + ")");
+				console.log("key (getProductValue) = " + key + "(" + typeof key + ")");
+				console.log(this.#productList[id][key]);
+			}
+			return this.#productList[id][key];
+		}
 
-		// getList(){ 
-		// 	return new Map(this.productList);
-		// };
+		getList(){ 
+			return JSON.parse(JSON.stringify(this.#productList));
+		};
 
 		search(val){
-			if (!this.productList.length)
+			if (!Object.keys(this.#productList).length)
 				return null;
 			val = val.toLowerCase();
-			let res = Array.from(this.productList).filter(el => el[1].name.toLowerCase().includes(val));
-			return new Map(res);
-		}
-
-		render(list = this.productList){
-			const parsePrice = function(num) {
-				return parseFloat(parseFloat(num).toFixed(2)).toLocaleString('en-US');
-			}
-			$(".product-list tbody").html("");
-			list.forEach((value, key, map) => {
-				$(".product-list tbody").append(`
-					<tr listId=${key}>
-						<td class="name align-middle" data-col-name="name">
-							<a class="name__info" href="">
-								${value.name}
-							</a>
-							<span class="name__count px-2 py-1">
-								${value.count}
-							</span>
-						</td>
-						<td class="price align-middle" data-col-name="price">
-							<div class="price__info">
-								$${parsePrice(value.price)}
-							</div>
-						</td>
-						<td class="align-middle" data-col-name="actions">
-							<div class="row justify-content-around">
-								<div class="col col-md-6 mb-1">
-									<button type="button" class="table__btn-edit [ btn btn-block btn-secondary ]">Edit</button>
-								</div>
-								<div class="col col-md-6">
-									<button type="button" class="table__btn-delete [ btn btn-block btn-outline-secondary ]">Delete</button>
-								</div>
-							</div>
-						</td>
-					</tr>`);
-			});
-
-			$(".table__btn-delete").on("click", function(){
-				const id = parseInt($(this).closest("tr").attr("listid"), 10);
-				$(".delete-modal__question").html(`Are you sure want to delete ${list.get(id).name}?`);
-				$(".delete-modal").attr("listid", id);
-				$(".delete-modal").fadeIn(300);
+			let result = {};
+			Object.entries(this.#productList).filter(el => el[1].name.toLowerCase().includes(val)).forEach((value) => {
+				result[value[0]] = value[1];
 			})
+			return result;
 		}
 	}
 
-	class Product{
-		// constructor(obj){
-		// 	this.name = obj.name;
-		// 	this.email = obj.email;
-		// 	//this.id = id();
-		// }
-
-		// sayHi (){
-		// 	console.log("Hi " + this.id);
-		// }
-
-		// clear(){
-		// 	for (let member in this) delete this[member];
-		// }
-	}
-
-	const validateForm = function(){
-
-	}
-
-	function onAddButton(){
-		let res = new Product();
-		$("")
-	}
-
-	function init(){
-		let list = new List;
-		let item = new Product;
-		let $form;
-		let onValidationError = false;
-
-		{
-			$(".table__input-search").attr("placeholder", "Enter product name...");
-			$(".add-modal__name").attr("placeholder", "Some product name");
-			$(".add-modal__email").attr("placeholder", "uss@epam.com");
+	class Table{
+		constructor(list = 0){
+			this.list = new List(list);
+			this.render();
+			this.initFields();
+			this.initModals();
 		}
 
-		{
-			$(".add-modal__delivery-country")
-			.html("")
-			.append(`<option selected disabled hidden></option>`)
-			.on("change", function(event) {
-				$(".select-container").html($(".select-container").find($(".select-all")));
-				$(".select-all").on("change", (event) => {
-					if ($(event.target).is(":checked")){
-						$(".add-modal__delivery-city").not(":checked").trigger("click");
-					}
-					else
-						$(".add-modal__delivery-city:checked").trigger("click");
-				})				
-				deliveryInfo
-				.get(event.target.value)
-				.forEach((val) => {
-					$(".select-container")
-					.append(`
-						<label class="">
-							<input class="add-modal__delivery-city" type="checkbox" name="delivery-city" value="${val}">
-							${val}
-						</label>
-					`);
+		#modals = {
+			common: $(".my-modal"),
+			edit: $(".edit-modal"),
+			delete: $(".delete-modal"),
+			info: $(".info-modal"),
+		}
+
+		#deliveryInfo = new Map([
+			[
+				"Russia",
+				["Moscow", "St. Petersburg", "Saratov", "Samara"]
+			],
+			[
+				"Belarus",
+				["Minsk", "Grodno", "Gomilev"]
+			],
+			[
+				"USA",
+				["New-York", "Chicago", "Siettle"]
+			]
+			]
+		);
+
+		#priceToEnStr = num => "$" + parseFloat(parseFloat(num).toFixed(2)).toLocaleString('en-US');
+		#priceFromEnStr = str => parseFloat(str.slice(1).replace(',',''));
+		#trimSpaces = str => str.replace(/^\s\s*/, '').replace(/\s\s*$/, '').replace(/\s\s*/, ' ');
+
+		initFields(){
+			((searchField) => {
+				const search = (obj) => {
+					$(obj).val((i, str) => this.#trimSpaces(str));
+					let _list = this.list.search($(obj).val());
+					if (_list)
+						this.render(_list);
+				}
+
+				const _input = $(searchField).find("input");
+
+				_input.attr("placeholder", "Enter product name...");
+
+				searchField
+				.on("click", "button.search", (event) => {
+					event.stopPropagation();
+					search(_input);
+				})
+				.on("keypress", "input", (event) => {
+					event.stopPropagation();
+					if (event.keyCode !== ENTER_KEY)
+						return;
+					event.preventDefault();
+					search(_input);
 				});
+			})($(".search-group"));
+
+			((addButton) => {
+				addButton.on("click", () => {
+					this.#modals.edit.data("id", -1)
+					this.#modals.edit.find(".js-modal-header").html("Add new product");
+					this.#modals.edit.fadeIn(300);
+				})
+			})($(".table__btn-add"));
+
+			((productTable) => {
+				const fillEdit = (id) => {
+					const country = this.list.getProductValue(id, "delivery-country");
+					console.log(country);
+					$(".js-edit-delivery-country")
+					.val(country);
+					this.#renderCities(country);
+					this.#modals.edit.find("input:not(.js-select-all)").each((i, elem) => {
+						$(elem).val(() => {
+							if (elem.name === "price")
+								return this.#priceToEnStr(this.list.getProductValue(id, elem.name));
+							return this.list.getProductValue(id, elem.name);
+						})
+					});
+
+				}
+
+				productTable
+				.on("click", ".js-btn-delete", (event) => {
+					const id = $(event.target).closest("tr").data("id");
+
+					$(".js-delete-question").html(`Are you sure want to delete ${this.list.getProductValue(id, "name")}?`);
+					this.#modals.delete
+					.data("id", id)
+					.fadeIn(300);
+				})
+				.on("click", ".js-btn-edit", (event) => {
+					const id = $(event.target).closest("tr").data("id");
+
+					this.#modals.edit.find(".js-modal-header").html(`Edit ${this.list.getProductValue(id, "name")}`);
+					fillEdit(id);
+					this.#modals.edit
+					.data("id", id)
+					.fadeIn(300);					
+				})
+			})($("table.product-list"));
+		}
+
+		initModals(){
+			this.#modals.delete
+			.on("click", ".js-delete-cancel", (event) => {
+				this.#modals.delete.fadeOut(300);
+			})
+			.on("click", ".js-delete-submit", (event) => {
+				this.#modals.delete.fadeOut(300);
+				this.list.deleteProduct(this.#modals.delete.data("id")).then((list) => {
+					this.render();
+				});
+
 			});
 
-			deliveryInfo.forEach((value, key, map) => {
-				$(".add-modal__delivery-country").append(`<option>${key}</option>`);
-			})
-		}
+			const onClose = (form) => {
+				form.reset();
+				$("input").removeClass("error");
+				$(form).find(".validation-error").html("");
+				$(".js-edit-delivery-city").parent().remove();
+			}
 
-		{
-			$(".select-container")
-			.html("")
-			.append(`
-				<label class="select-all">
-					<input class="add-modal__delivery-city" type="checkbox" name="delivery-city" value="select-all">
-					Select all
-				</label>
-			`);
-			$(".select-all").on("change", (event) => {
-				if ($(event.target).is(":checked"))
-					$(".add-modal__delivery-city").not(":checked").change();
-			})
-		}
-
-		{
-			const validateInput = function(elem){
-				const _validate = function(value, name){
+			const validateInput = (elem) => {
+				const _validate =(value, name) => {
 					switch (name) {
 						case "name":
 							if (value.length < 5)
@@ -273,138 +251,204 @@ $(document).ready(function(){
 							return "";
 					}
 				}
-				const errorMsg = _validate(elem.value, elem.name);
+				let _value = elem.value.slice(0);
+				const _name = elem.name.slice(0);
+				if(_name === "price" && _value.slice(0, 1) === "$")
+					_value = this.#priceFromEnStr(_value);
+				const errorMsg = _validate(_value, _name);
 				if (errorMsg === ""){
-					console.log(elem);
 					$(elem)
 					.removeClass("error")
-					.parent()
-					.children(".validation-error").html("");
+					.siblings(".validation-error").html("");
 					return $(elem).val();
 				}
 				$(elem)
 				.addClass("error")
-				.parent()
-				.children(".validation-error").html(errorMsg);
+				.siblings(".validation-error").html(errorMsg);
 				return null;
 			}
 
-			const formatPrice = function(num) {
-				return "$" + parseFloat(parseFloat(num).toFixed(2)).toLocaleString('en-US');
-			}
+			((editForm) => {
+				editForm.find(".js-edit-delivery-country")
+				.html("")
+				.append(`<option selected disabled hidden></option>`)
+				.on("change", (event) => {
+					this.#renderCities(event.target.value);
+				});
 
-			$("[validationNeeded]")
-			.on("blur", (event) => {
-				if (validateInput(event.target) && event.target.name === "price")
-					event.target.value = formatPrice(event.target.value);
-			})
+				this.#deliveryInfo.forEach((value, key, map) => {
+					$(".js-edit-delivery-country").append(`<option>${key}</option>`);
+				})
+			})($(this.#modals.edit.find("form")[0]));
 
-			$("form").on("focus", ".error", (event) => {
-				event.target.value = "";
+			this.#modals.edit
+			.on("click", ".js-btn-close", () => {
+				this.#modals.common.fadeOut(300);
+				onClose(this.#modals.edit.find("form")[0]);				
 			})
-			
-			$(".add-modal__price:not(.error)").on("focus", (event) => {
-				if (event.target.value)
-					event.target.value = parseFloat(event.target.value.slice(1).replace(',',''));
+			.on("click", ".js-edit-cancel", (event) => {
+				this.#modals.common.fadeOut(300);
+				onClose(this.#modals.edit.find("form")[0]);		
 			})
-
-			$(".add-modal__count")
-			.on("keypress", (event) => {
-				if (event.which != 8 && event.which != 0 && event.which != 46 && (event.which < 48 || event.which > 57))
-					return false;
-				return true;
-			})
-			.on("paste", (event) => {
-				if(!parseInt(event.originalEvent.clipboardData.getData('Text')))
-					return false;
-				return true;
-			})
-
-			$(".add-modal__btn-add").on("click", (event) => {
-				let res = new Product({});
-				onValidationError = false;
+			.on("click", ".js-edit-submit", (event) => {
+				let res = {};
+				let onValidationError = false;
 				let curRes;
-				$("#form-add [validationNeeded]").each((i, elem) => {
-					curRes = validateInput(elem);
+				this.#modals.edit.find('input[type=text]').each((i, elem) => {
+					$(elem).val(() => this.#trimSpaces(elem.value));
+					curRes = $(elem).hasClass("js-validate")
+						? validateInput(elem)
+						: elem.value;
 					if (!curRes)
 						onValidationError = true;
 					else
-						res[elem.name] = validateInput(elem);
-				});		
-				$(".add-modal__delivery-city:checked").each((i, elem) => {
+						res[elem.name] = curRes;
+				});	
+				if (res.hasOwnProperty("price"))
+					res.price = this.#priceFromEnStr(res.price);
+				res["delivery-country"] = $(".js-edit-delivery-country").val();
+				$(".js-edit-delivery-city:checked").each((i, elem) => {
 					if (!res.hasOwnProperty(elem.name))
 						res[elem.name] = [];
 					res[elem.name].push($(elem).val());
 				})
+				console.log(res);
 				if (!onValidationError){
-					list.addProduct(res).then((list) => {
-						list.render();
-						$form[0].reset();
-						$(".my-modal").fadeOut(300);
+					this.#modals.common.fadeOut(300);
+					this.list.addProduct(res, this.#modals.edit.data("id")).then((list) => {
+						this.render();
+						onClose(this.#modals.edit.find("form")[0]);
 					})
 				}
 			})
-		}
-
-		$(".form__btn-close").on("click", () => {
-			$form[0].reset();
-			$(".validation-error").html("");
-			$(".my-modal").fadeOut(300);
-		})
-
-		$(".table__btn-add").on("click", () => {
-			$(".add-modal").fadeIn(300);
-			$form = $("#form-add")
-		})
-
-
-		$(".add-modal__btn-cancel").on("click", () => {
-			$form[0].reset();
-			$(".validation-error").html("");
-			$(".add-modal").fadeOut(300);
-		})
-
-		$(".delete-modal__btn-no").on("click", () => {
-			$(".delete-modal").fadeOut(300);
-		})
-
-		$(".delete-modal__btn-yes").on("click", (event) => {
-			list.deleteProduct($(".delete-modal").attr("listid")).then((list) => {
-				$("body").css("cursor", "default");
-				$(".delete-modal").fadeOut(300);
-				list.render();
-			});
-		})
-
-		{
-			const search = (obj) => {
-				let _list = list.search($(obj).val());
-				if (_list)
-					list.render(_list);
-			}
-			$(".table__btn-search").on("click", () => {
-				search($(".table__input-search"));
-			});
-			$(".table__input-search").on("keypress", (event) => {
-				if (event.keyCode !== ENTER_KEY)
-					return;
-				event.preventDefault();
-				search($(".table__input-search"));
+			.on("change", ".select-all", (event) => {
+				if ($(event.target).is(":checked")){
+					$(".js-edit-delivery-city").not(":checked").prop("checked", true);
+				}
+				else
+					$(".js-edit-delivery-city:checked").prop("checked", false);
 			})
+			.on("change", ".js-edit-delivery-city", (event) => {
+				if ($(event.target).is(":checked")){
+					if(!$(".js-edit-delivery-city").not(":checked").length)
+						$(".js-select-all").prop("checked", true);
+				}
+				else
+					$(".js-select-all").prop("checked", false);
+			})
+			.on("blur", ".js-validate:not(.js-edit-price)", (event) => {
+				event.target.value = this.#trimSpaces(event.target.value);
+				validateInput(event.target);
+			})
+			.on("blur", ".js-edit-price", (event) => {
+				event.target.value = this.#trimSpaces(event.target.value);
+				if (validateInput(event.target))
+					event.target.value = this.#priceToEnStr(event.target.value);
+			})
+			.on("focus", "form .error", (event) => {
+				event.target.value = "";
+			})
+			.on("focus", ".js-edit-price:not(.error)", (event) => {
+				event.target.value = event.target.value && this.#priceFromEnStr(event.target.value);
+			})
+			.on("input", ".js-edit-count", (event) => {
+				$(event.target).val((i, str) => str.replace(/[^0-9]/g, ''));
+			});
 			
 		}
 
-		{	
-			
+		#renderCities = (country) => {
+			$(".js-edit-delivery-city").parent().remove();
+			this.#deliveryInfo
+			.get(country)
+			.forEach((val) => {
+				$(".select-container")
+				.append(`
+					<label>
+						<input class="js-edit-delivery-city" type="checkbox" name="delivery-city" value="${val}">
+						${val}
+					</label>
+				`);
+			});
 		}
 
-		{
-			$(".name__info").on("click", (event) => {
-				$(".info-modal").fadeIn(300);
-			})
+		render(list = this.list.getList()){
+			$(".product-list tbody").html("");
+			for (let id in list){
+			//list.forEach((value, key, map) => {
+				$(".product-list tbody").append(`
+					<tr data-id=${id}>
+						<td class="name align-middle" data-col-name="name">
+							<a class="name__info" href="">
+								${list[id].name}
+							</a>
+							<span class="name__count px-2 py-1">
+								${list[id].count}
+							</span>
+						</td>
+						<td class="price align-middle" data-col-name="price">
+							<div class="price__info">
+								${this.#priceToEnStr(list[id].price)}
+							</div>
+						</td>
+						<td class="align-middle" data-col-name="actions">
+							<div class="row justify-content-around">
+								<div class="col col-md-6 mb-1">
+									<button type="button" class="js-btn-edit [ btn btn-block btn-secondary ]">Edit</button>
+								</div>
+								<div class="col col-md-6">
+									<button type="button" class="js-btn-delete [ btn btn-block btn-outline-secondary ]">Delete</button>
+								</div>
+							</div>
+						</td>
+					</tr>`
+				);
+			}//)
 		}
-
 	}
 
-	init();
+	function main(){
+		const initialTable = {};
+		// let prod1 = new Product;
+		// 	prod1["name"]= "Pencil";
+		// 	prod1["email"]= "uss@gmail.com";
+		// 	prod1["count"]= 7;
+		// 	prod1["price"]= 5432.6533;
+		// 	prod1["delivery-country"]= ["Russia"];
+		// 	prod1["delivery-city"]= ["Saratov", "Moscow"];
+		// initialTable["1"] = prod1;
+		// console.log(initialTable["1"]["delivery-country"]);
+		let prod1 = {
+			name: "Pencil",
+			email: "abcfs12@gmail.com",
+			count: 654,
+			price: 4322.45,
+			"delivery-country": "Russia",
+			"delivery-city": ["Saratov", "Moscow"]
+		};
+		initialTable["1"] = prod1;
+		let prod2 = {
+			name: "Pen01",
+			email: "abcfs12@gmail.com",
+			count: 443,
+			price: 7434322.654,
+			"delivery-country": "Belarus",
+			"delivery-city": ["Minsk"]
+		};
+		initialTable["2"] = prod2;
+		let prod3 = {
+			name: "Adonit",
+			email: "asfbf@gmail.com",
+			count: 467,
+			price: 6543.00000,
+			"delivery-country": "Russia",
+			"delivery-city": ["Saratov", "Moscow"]
+		};
+		initialTable["3"] = prod3;
+
+		const tbl = new Table(initialTable);
+		document.tbl = tbl;
+	}
+
+	main();
 })
