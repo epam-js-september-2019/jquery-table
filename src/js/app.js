3763b5609a9fa69a41371105e2fe859525bd4fc5
Max Afanasev <maxfoxcg@gmail.com>
2019-11-07 03:29:13 +0300

    Add reactive model

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,19 @@
+import { ProductsModel } from "./productsModel.js";
 import { ProductsList } from "./productsList.js";
-import { EditForm } from "./editForm.js";
 import { AddForm } from "./addForm.js";
 import { RemoveDialog } from "./removeDialog.js";
 
 export class App {
   constructor() {
+    this.model = new ProductsModel();
     this.table = new ProductsList({
       showDetails: this.showDetails.bind(this),
       edit: this.editProduct.bind(this),
       remove: this.removeProduct.bind(this)
     });
-    console.log("App has been initialized");
-    this.table.render({ items: PRODUCTS });
+    this.table.render({ items: this.model.getAll() });
+    this.model.subscribe(data => this.table.render({ items: data }));
+    this.model.init();
   }
   showDetails(id) {
     console.log("Show details: " + id);
@@ -23,11 +25,3 @@ export class App {
     console.log("Show details " + id);
   }
 }
-
-const PRODUCTS = [
-  { id: 1, name: "Antarcticite", amount: 5, price: 1115 },
-  { id: 2, name: "Brookite", amount: 72, price: 782 },
-  { id: 3, name: "Chrysocolla", amount: 5, price: 880 },
-  { id: 4, name: "Heulandite", amount: 77, price: 944 },
-  { id: 5, name: "Pyroxene", amount: 115, price: 1120 }
-];
