d0c7ad84c125568c089554d27aa121e5f388a0fc
Elena Suiazova <48860826+Sokvadora@users.noreply.github.com>
2019-11-08 15:31:23 +0300

    fix bugs for checkbox

--- index.js
+++ index.js
@@ -393,20 +393,18 @@ let arrForDelivery = [];
 function selectCity() {
   $("#select-country").change(function() {
     selectDelivery();
+    $("#select-all").change(function()  {
+      var group = $(this).attr('data-group');
+      $('.one[data-group="' + group + '"]').prop("checked", this.checked);
+  });
 
-    $("input:checkbox").change(function() {
-      const selectCity = $(this).next().text();
-      if (selectCity === "Select all") {
-        $(":checkbox").each(function() {
-          this.checked = true;
-        });
-      } else {
-        $(":checkbox").each(function() {
-          this.checked = false;
-        });
-        this.checked = true;
-      }
-    });
+  $(".one").change(function()  {
+      var group = $(this).attr('data-group');
+      var allChecked = $('.one[data-group="' + group + '"]:not(:checked)').length == 0;
+      $('.all[data-group="' + group + '"]').prop("checked", allChecked);
+      console.log($("input[type='checkbox']").text())
+  });
+     
   });
 }
 
