fd54bbd3fbd451969b7c57a99aa2d7fb592fa6ce
Andrey Kim <kimoofey@gmail.com>
2019-11-09 04:04:31 +0300

    All functions are working well. Validating, creating, editing, deleting.

--- scripts/custom.js
+++ scripts/custom.js
@@ -115,6 +115,20 @@ function deleteProduct() {
     });
 }
 
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => table.appendChild(tr) );
+    $("i").removeClass("fa-caret-down").addClass("fa-caret-up");
+})));
+
 async function addProductInTable(arg) {
     $("tbody tr ").hide();
     let dataForItemTemplate = product => {
